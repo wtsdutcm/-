a6a309cf41d0ccae36e72470147df60c04215e37
wtsdutcm <wtsdutcm@139.com>
2021-09-12 23:10:26 +0800

    first commit

---
+++ TempConvert2.py
@@ -0,0 +1,10 @@
+tempstr=input('请输入带有符号的温度值：')
+if tempstr[0] in ['F','f']:
+    C=(eval(tempstr[1: ])-32)/1.8
+    print('转换后的温度是C{:.2f}'.format(C))    
+elif tempstr[0] in ['C','c']:
+    F=1.8*eval(tempstr[1: ])+32
+    print("转换后的温度是F{:.2f}".format(F))
+    
+else:
+    print('输入格式错误！')
